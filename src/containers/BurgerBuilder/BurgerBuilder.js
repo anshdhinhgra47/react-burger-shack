76c528446810ef7d764bde29d9798843c1ff0ae3
Ansh Dhingra <56676059+anshdhinhgra47@users.noreply.github.com>
2021-04-03 23:16:54 +0530

    Added less and disabled feature

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -37,15 +37,38 @@ class BurgerBuilder extends Component {
     }
 
     removeIngredient = (type) => {
+        const oldCount = this.state.ingredients[type];
+        if(oldCount <= 0) {
+            return;
+        }
+        const newCount = oldCount - 1;
+        const updateIngredients = {
+            ...this.state.ingredients // distributes the properties of the old ingredient state into the new one
+        };
+
+        updateIngredients[type] = newCount;
+        const priceDeletion = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice - priceDeletion;
+
+        this.setState({totalPrice: newPrice, ingredients: updateIngredients});
 
     }
 
     render() {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+        for(let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        }
         return(
             <Aux>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
-                ingredientAdded={this.addIngredient} />
+                ingredientAdded={this.addIngredient} 
+                ingredientRemoved={this.removeIngredient}
+                disabled={disabledInfo} />
             </Aux>
         );
     }
