6dd37283d83cec968c040d70dc32a0665bf9edf5
Ansh Dhingra <56676059+anshdhinhgra47@users.noreply.github.com>
2021-04-03 16:46:18 +0530

    Adding ingredients dynamically

--- src/components/Burger/BurgerIngredient/Burger.js
+++ src/components/Burger/BurgerIngredient/Burger.js
@@ -3,11 +3,16 @@ import './Burger.css';
 import BurgerIngredient from './BurgerIngredient';
 
 const Burger = (props) => {
+    const new_ingredients = Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />; 
+        }); // Array of 2 elements
+    });
+
     return (
         <div className="burger">
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {new_ingredients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,10 +3,19 @@ import Burger from '../../components/Burger/BurgerIngredient/Burger';
 import Aux from '../../hoc/Aux';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
     render() {
         return(
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients} />
                 <div>All the Burger Ingredients</div>
             </Aux>
         );
