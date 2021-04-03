dae955eca2c46a98fbcf8da7300c962de4442bf8
Ansh Dhingra <56676059+anshdhinhgra47@users.noreply.github.com>
2021-04-03 21:44:44 +0530

    handled case of 0 ingredients

--- src/components/Burger/BurgerIngredient/Burger.js
+++ src/components/Burger/BurgerIngredient/Burger.js
@@ -3,11 +3,19 @@ import './Burger.css';
 import BurgerIngredient from './BurgerIngredient';
 
 const Burger = (props) => {
-    const new_ingredients = Object.keys(props.ingredients).map(igKey => {
+    let new_ingredients = Object.keys(props.ingredients).map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_, i) => {
             return <BurgerIngredient key={igKey + i} type={igKey} />; 
-        }); // Array of 2 elements
-    });
+        }); // Fetches the values of the ingredients and converts them in the array
+            // to be applied between the top & bottom burger buns
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el);
+    }, []); // Concatenating all the array elements in one single array
+
+    if(new_ingredients.length == 0) {
+        new_ingredients = <p>Please add some ingredients!</p>;
+    }
 
     return (
         <div className="burger">
